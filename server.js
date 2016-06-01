ea2494fd3d7db93d874afde349f58817a53cf2e8
Chris Perez <chris.k.perez@gmail.com>
2016-06-01 10:32:48 -0700

    skeletoned

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const app = require('express')();
+const authRoutes = require(__dirname + '/route/auth_routes');
+const mongoose = require ('mongoose');
+
+mongoose.connect('mongodb://localhost/auth_db');
+
+//app.use('/', authRoutes);
+
+app.listen(3000, ()=>{console.log('Server way the fudge up on THREE THOUSAND!')})
